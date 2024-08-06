8dae92be4924efbf8a96a9cf5e1bb3238adb9142
sunny <sunnyeyles@gmail.com>
2024-08-06 13:18:08 +0200

    Downgraded ts version

--- api-client/createRecipeAndRetrieve.ts
+++ api-client/createRecipeAndRetrieve.ts
@@ -1,6 +1,5 @@
 import axios from 'axios'
 import { RecipeType } from '@/types/mainTypes'
-import { useEffect } from 'react'
 export const createRecipeAndRetrieve = async (recipe: RecipeType) => {
   try {
     await axios.put('/save-generated-recipe', {
