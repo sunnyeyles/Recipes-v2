68223657b6ed522f8b82967f8d3cc082f017965d
sunny <sunnyeyles@gmail.com>
2024-08-06 15:17:42 +0200

    New feature: added new rules to commit messages

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
   rules: {
     'header-min-length': [2, 'always', 20],
     'header-case-start-capital': [2, 'always'],
-    // 'header-end-period': [2, 'always'],
+    'header-type-required': [2, 'always'],
   },
   plugins: [
     {
@@ -11,21 +11,26 @@ module.exports = {
         'header-case-start-capital': (parsed) => {
           return [
             /^[A-Z]/.test(parsed.raw),
-            'The commit message has to start with a capital letter',
+            'The commit message has to start with a capital letter. - I know I sound like an English teacher...',
           ]
         },
         'header-min-length': (parsed) => {
           return [
             parsed.raw.length >= 20,
-            'Be more descriptive, you cannot use a 2 letter commit',
+            'Be more descriptive, your commit message has to be more than 20 chars.',
           ]
         },
-        // 'header-end-period': (parsed) => {
-        //   return [
-        //     /\.$/.test(parsed.raw),
-        //     'Please end the sentence with a full stop! (.)',
-        //   ]
-        // },
+        'header-type-required': ({ header }) => {
+          const validTypes = ['bug fix', 'new feature', 'generic']
+          const hasValidType = validTypes.some((type) =>
+            header.toLowerCase().includes(type)
+          )
+
+          return [
+            hasValidType,
+            `Commit message must include: bug fix, new feature, or just include generic to opt out of it.`,
+          ]
+        },
       },
     },
   ],
